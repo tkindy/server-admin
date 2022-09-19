e3c8d3d8f0cfde4266ba330708a2a130f49499d1
Tyler Kindy <tylerkindy@gmail.com>
2022-09-18 20:31:45 -0400

    Add Caddy config API function

---
+++ src/com/tylerkindy/server_admin/caddy.clj
@@ -0,0 +1,20 @@
+(ns com.tylerkindy.server-admin.caddy
+  (:require [clj-http.client :as client]))
+
+(defn get-config []
+  (-> (client/get "http://localhost:2019/config" {:as :json})
+      :body))
+
+(defn build-config-uri [id path]
+  (str "http://localhost:2019/"
+       (if id
+         (str "id/" id "/")
+         "config/")
+       path))
+
+(defn post-config
+  ([path value] (post-config nil path value))
+  ([id path value]
+   (client/post (build-config-uri id path)
+                {:body value
+                 :content-type :json})))
